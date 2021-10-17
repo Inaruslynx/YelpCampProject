607fd2db2ec82010c4479d9f7f531cf1c3621f6f
Inaruslynx <inaruslynx@gmail.com>
2021-10-17 00:16:00 +0000

    Finished user register

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const tryAsync = require("../utils/tryAsync");
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", async (req, res) => {
-  const { username, password, email } = req.body;
-  const user = new User({ username, email });
-  const registedUser = await User.register(user, password);
-  console.log(registedUser);
-  req.flash("Welcome to Yelp Camp!");
-  res.redirect("/campgrounds");
-});
+router.post(
+  "/register",
+  tryAsync(async (req, res) => {
+    try {
+      const { username, password, email } = req.body;
+      const user = new User({ username, email });
+      const registedUser = await User.register(user, password);
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      req.flash('error', e.message)
+      res.redirect('register')
+    }
+  })
+);
 
 module.exports = router;
