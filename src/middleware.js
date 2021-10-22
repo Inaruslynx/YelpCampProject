883cede29f747189202c194bdda87fa069ee9b80
Inaruslynx <inaruslynx@gmail.com>
2021-10-22 23:58:53 +0000

    Trying to add a redirect function

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,10 @@
 module.exports.isLoggedIn = function (req, res, next) {
-    if (!req.isAuthenticated()) {
-        req.flash('error', 'You must be logged in')
-        return res.redirect('/login')
-    }
-    next();
-}
+  if (!req.isAuthenticated()) {
+    console.log(req.originalUrl);
+    // Something is wrong here
+    res.session.returnTo = req.originalUrl;
+    req.flash("error", "You must be logged in");
+    return res.redirect("/login");
+  }
+  next();
+};
