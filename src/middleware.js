835b874cb5248719d5a47dd13ed8943cabd13c8f
Inaruslynx <inaruslynx@gmail.com>
2021-10-31 21:36:26 +0000

    accidentally used res instead of req

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,7 @@
 module.exports.isLoggedIn = function (req, res, next) {
   if (!req.isAuthenticated()) {
     console.log(req.originalUrl);
-    // Something is wrong here
-    res.session.returnTo = req.originalUrl;
+    req.session.returnTo = req.originalUrl;
     req.flash("error", "You must be logged in");
     return res.redirect("/login");
   }
