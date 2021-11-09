7711760d6369d3d50e6bd4954fe96380702b5f7e
Inaruslynx <inaruslynx@gmail.com>
2021-11-09 00:39:31 +0000

    Added basic user function to campgrounds

--- src/routes/campgrounds.js
+++ src/routes/campgrounds.js
@@ -56,6 +56,7 @@ router.post(
         },
       };
       const campground = new Campground(params);
+      campground.author = req.user._id;
       await campground.save();
       req.flash("success", "Successfully created new campground!");
       res.redirect(`/campgrounds/${campground._id}`);
@@ -126,9 +127,9 @@ router.delete(
 router.get(
   "/:id",
   tryAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
+    const campground = await Campground.findById(req.params.id)
+      .populate("reviews")
+      .populate("author");
     if (!campground) {
       req.flash("error", "Cannot find that campground!");
       return res.redirect("/campgrounds");
