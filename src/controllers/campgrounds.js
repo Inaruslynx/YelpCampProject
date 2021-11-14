fdc6c4e2290ee29910f043a73104e0b927b03f51
Inaruslynx <inaruslynx@gmail.com>
2021-11-14 21:16:49 +0000

    Created controller and moved route logic into it

---
+++ src/controllers/campgrounds.js
@@ -0,0 +1,114 @@
+const Campground = require("../models/campgrounds");
+
+// Home page
+module.exports.index = async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+};
+
+// Returns form for new campground
+module.exports.newCampground = (req, res) => {
+  res.render("campgrounds/new");
+};
+
+// Submits new campground
+module.exports.submitNewCampground = async (req, res) => {
+  let params = req.body.campground;
+  const urlArray = params["image"].split("/");
+  const photoId = urlArray[urlArray.length - 1];
+  const results = await getData(photoId);
+  if (results.type === "success") {
+    const photo = results.response;
+    params["image"] = {
+      id: photo.id,
+      width: photo.width,
+      height: photo.height,
+      urls: {
+        raw: photo.urls.raw,
+        full: photo.urls.full,
+        regular: photo.urls.regular,
+        small: photo.urls.small,
+        thumb: photo.urls.thumb,
+      },
+    };
+    const campground = new Campground(params);
+    campground.author = req.user._id;
+    await campground.save();
+    req.flash("success", "Successfully created new campground!");
+    res.redirect(`/campgrounds/${campground._id}`);
+  } else {
+    console.log("Error getting photo data");
+    console.log(results);
+  }
+};
+
+// Edit campground
+module.exports.submitEditCampground = async (req, res) => {
+  let data = req.body.campground;
+  const { id } = req.params;
+  const camp = await Campground.findById(id);
+  if (camp.image.urls.raw === data.image) {
+    delete data.image;
+    await Campground.findByIdAndUpdate(id, data, {
+      new: true,
+      runValidators: true,
+    });
+  } else {
+    let url = data.image.split("/");
+    const photoId = url[url.length - 1];
+    const results = await getData(photoId);
+    if (results.type === "success") {
+      const photo = results.response;
+      data["image"] = {
+        id: photo.id,
+        width: photo.width,
+        height: photo.height,
+        urls: {
+          raw: photo.urls.raw,
+          full: photo.urls.full,
+          regular: photo.urls.regular,
+          small: photo.urls.small,
+          thumb: photo.urls.thumb,
+        },
+      };
+      await Campground.findByIdAndUpdate(id, data, {
+        new: true,
+        runValidators: true,
+      });
+    }
+  }
+  req.flash("success", "Successfully updated campground!");
+  res.redirect(`/campgrounds/${id}`);
+};
+
+// Delete campground
+module.exports.deleteCampground = async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndRemove(id);
+  req.flash("success", "Successfully deleted campground!");
+  res.redirect("/campgrounds");
+};
+
+// Show a specific campground
+module.exports.showCampground = async (req, res) => {
+  const campground = await Campground.findById(req.params.id)
+    .populate({ path: "reviews", populate: { path: "author" } }) // Populate reviews and their authors
+    .populate("author"); // Populate author of campground
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
+
+// Renders campground edit
+module.exports.editCampground = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/campgrounds");
+  }
+  const url = campground.image.urls.raw;
+  res.render(`campgrounds/edit`, { campground, url });
+};
--- src/routes/campgrounds.js
+++ src/routes/campgrounds.js
@@ -1,58 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const Campground = require("../models/campgrounds");
+const campground = require("../controllers/campgrounds");
+// const Campground = require("../models/campgrounds");
 const { getData } = require("../utils/unsplash");
-const { tryAsync } = require("../utils/tryAsync")
+const { tryAsync } = require("../utils/tryAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
 // Home page
-router.get(
-  "/",
-  tryAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds });
-  })
-);
+router.get("/", tryAsync(campground.index));
 
 // Returns form for new campground
-router.get("/new", isLoggedIn, (req, res) => {
-  res.render("campgrounds/new");
-});
+router.get("/new", isLoggedIn, campground.newCampground);
 
 // Submits new campground
 router.post(
   "/",
   isLoggedIn,
   validateCampground,
-  tryAsync(async (req, res) => {
-    let params = req.body.campground;
-    const urlArray = params["image"].split("/");
-    const photoId = urlArray[urlArray.length - 1];
-    const results = await getData(photoId);
-    if (results.type === "success") {
-      const photo = results.response;
-      params["image"] = {
-        id: photo.id,
-        width: photo.width,
-        height: photo.height,
-        urls: {
-          raw: photo.urls.raw,
-          full: photo.urls.full,
-          regular: photo.urls.regular,
-          small: photo.urls.small,
-          thumb: photo.urls.thumb,
-        },
-      };
-      const campground = new Campground(params);
-      campground.author = req.user._id;
-      await campground.save();
-      req.flash("success", "Successfully created new campground!");
-      res.redirect(`/campgrounds/${campground._id}`);
-    } else {
-      console.log("Error getting photo data");
-      console.log(results);
-    }
-  })
+  tryAsync(campground.submitNewCampground)
 );
 
 // Edit campground
@@ -61,43 +26,7 @@ router.put(
   isLoggedIn,
   isAuthor,
   validateCampground,
-  tryAsync(async (req, res) => {
-    let data = req.body.campground;
-    const { id } = req.params;
-    const camp = await Campground.findById(id);
-    if (camp.image.urls.raw === data.image) {
-      delete data.image;
-      await Campground.findByIdAndUpdate(id, data, {
-        new: true,
-        runValidators: true,
-      });
-    } else {
-      let url = data.image.split("/");
-      const photoId = url[url.length - 1];
-      const results = await getData(photoId);
-      if (results.type === "success") {
-        const photo = results.response;
-        data["image"] = {
-          id: photo.id,
-          width: photo.width,
-          height: photo.height,
-          urls: {
-            raw: photo.urls.raw,
-            full: photo.urls.full,
-            regular: photo.urls.regular,
-            small: photo.urls.small,
-            thumb: photo.urls.thumb,
-          },
-        };
-        await Campground.findByIdAndUpdate(id, data, {
-          new: true,
-          runValidators: true,
-        });
-      }
-    }
-    req.flash("success", "Successfully updated campground!");
-    res.redirect(`/campgrounds/${id}`);
-  })
+  tryAsync(campground.submitEditCampground)
 );
 
 // Delete campground
@@ -105,44 +34,18 @@ router.delete(
   "/:id",
   isLoggedIn,
   isAuthor,
-  tryAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndRemove(id);
-    req.flash("success", "Successfully deleted campground!");
-    res.redirect("/campgrounds");
-  })
+  tryAsync(campground.deleteCampground)
 );
 
 // Show a specific campground
-router.get(
-  "/:id",
-  tryAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-      .populate("reviews")
-      .populate("author");
-    if (!campground) {
-      req.flash("error", "Cannot find that campground!");
-      return res.redirect("/campgrounds");
-    }
-    res.render("campgrounds/show", { campground });
-  })
-);
+router.get("/:id", tryAsync(campground.showCampground));
 
 // Renders campground edit
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
-  tryAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-      req.flash("error", "Cannot find that campground!");
-      return res.redirect("/campgrounds");
-    }
-    const url = campground.image.urls.raw;
-    res.render(`campgrounds/edit`, { campground, url });
-  })
+  tryAsync(campground.editCampground)
 );
 
 module.exports = router;
