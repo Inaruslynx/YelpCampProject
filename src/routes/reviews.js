b1529ec4548d03b8f48f82def0549282bbfaaf20
Inaruslynx <inaruslynx@gmail.com>
2021-11-14 19:40:35 +0000

    Added author to reviews

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -13,6 +13,7 @@ router.post(
   tryAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
